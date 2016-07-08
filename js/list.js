907e487112436c96e884ef39a75be4be4596682a
Valera Zemlanikin <doggvalera@gmail.com>
2016-07-08 15:16:42 +0300

    frist commit

---
+++ js/list.js
@@ -0,0 +1,53 @@
+function loadUsers() {
+    return fetch('http://localhost:8080/students')
+        .then(function (response) {
+            return response.json();
+        });
+}
+
+function drawUserList() {
+    loadUsers().then(function (users) {
+        var userListTemplate = Handlebars.compile(document.querySelector('#user-list').innerHTML);
+        var userTemplate = Handlebars.compile(document.querySelector('#user').innerHTML);
+
+        var userList = '';
+        users.forEach(function (user) {
+            userList += userTemplate(user);
+        });
+
+        var userList = userListTemplate({
+            body: userList
+        });
+
+
+        var userListContainer = document.createElement('div');
+        userListContainer.innerHTML = userList;
+        document.querySelector(".list").appendChild(userListContainer);
+    });
+}
+
+function handleSubmit() {
+
+    event.preventDefault();
+
+    var payload = {
+        name: document.user.firstname.value,
+        surname: document.user.surname.value
+    };
+
+    fetch("http://localhost:8080/students",
+        {
+            method: "POST",
+            body: JSON.stringify(payload),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then(function () {
+            var qs = document.querySelector(".list");
+            qs.removeChild(qs.lastElementChild);
+            drawUserList();
+        });
+}
+
+drawUserList();
